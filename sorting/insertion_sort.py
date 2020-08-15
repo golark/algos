4acc7478344d40403cae733289016200c399f6a0
golark <firatcnur@gmail.com>
2020-08-15 22:49:33 +0300

    added insertion sort

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,24 @@
+
+
+# selection_sort
+def insertion_sort(arr):
+
+    for i in range(1, len(arr)):
+
+        # find the correct location by swapping
+        idx = i
+        while idx > 0 and arr[idx] < arr[idx-1]:
+            arr[idx-1], arr[idx] = arr[idx], arr[idx-1]
+            idx = idx - 1
+
+
+def main():
+
+    arr = [5,6,8,1,2,4,5,10]
+    insertion_sort(arr)
+
+    print(f'{arr}')
+
+
+if __name__ == "__main__":
+    main()
--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,4 +1,3 @@
-import math
 
 
 # selection_sort
